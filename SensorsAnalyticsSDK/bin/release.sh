0f8d426cd2640c9eeb8d6cf4ce5b4fcf7a5d3215
Jack Caperon <jack@canva.com>
2020-12-18 15:02:13 +1000

    S3 Upload Scripts/POM (#2)

---
+++ SensorsAnalyticsSDK/bin/release.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+cd "${REPO_ROOT}"
+
+SENSORS_HOME="${REPO_ROOT}/SensorsAnalyticsSDK"
+SNAPSHOT_HOME="${SENSORS_HOME}/target/snapshot"
+
+main() {
+
+  cd "${SENSORS_HOME}"
+
+  if [[ -z "${S3_REPOSITORY_URL:-}" ]]; then
+    echo "S3_REPOSITORY_URL not set"
+    exit 1
+  fi
+
+  local profile="$1"
+
+  echo "--- Building artifacts"
+
+  rm -rf "${SNAPSHOT_HOME}"
+
+  mvn \
+    deploy \
+    -DaltDeploymentRepository=snapshot-repo::default::file:target/snapshot
+
+  echo "--- Deploying maven artifact to S3"
+
+  aws s3 cp \
+    --profile "${profile}" \
+    "${SNAPSHOT_HOME}" \
+    "${S3_REPOSITORY_URL}" \
+    --recursive \
+    --exclude "**/*/maven-metadata.xml*" \
+    --acl "bucket-owner-full-control"
+
+  echo "--- Success"
+}
+
+main "$@"
